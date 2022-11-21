db266eff9b490c497b2daf37ecdd65c859e12ddf
Marco Chi Chung Fong <marco.fong.hk@gmail.com>
2022-11-21 19:39:42 +0100

    added search operators

--- main.py
+++ main.py
@@ -18,6 +18,18 @@ def load_credentials():
 if __name__ == '__main__':
     f = load_credentials()
     data = json.load(f)
+
+    client = tweepy.Client(data['bearer_token'])
+
+    query_string = 'migratie -is:retweet lang:nl place_country:BE'
+    tweets = client.search_all_tweets(query=query_string,start_time = "2019-09-29T18:46:19Z",end_time = "2020-10-29T18:46:19Z",max_results=500)
+    print(len(tweets.data))
+
+    for tweet in tweets.data:
+        print(tweet.text)
+
+
+
     #twitter = Twython(data['api_key'], data['api_key_secret'], oauth_version=2)
     #ACCESS_TOKEN = twitter.obtain_access_token()
     #twitter = Twython(data['api_key'], access_token=ACCESS_TOKEN)
@@ -31,7 +43,7 @@ if __name__ == '__main__':
     api = tweepy.API(auth)
     
     """
-
+    '''
     auth = tweepy.OAuth2UserHandler(
         consumer_key=data['api_key'],
         consumer_secret=data['api_key_secret'],
@@ -39,6 +51,7 @@ if __name__ == '__main__':
         access_token_secret=data['access_token_secret']
     )
     print(auth)
+    '''
     '''
     client = tweepy.Client(bearer_token='REPLACE_ME')
 
