d0875bb54c72e3022060ac9883a205825de73efe
Marco Chi Chung Fong <marco.fong.hk@gmail.com>
2023-02-16 20:04:32 +0100

    process_tweet function added

--- main.py
+++ main.py
@@ -14,25 +14,26 @@ def load_credentials():
     cred = open('credentials/twitter_credentials.json')
     return cred
 
+def query_tweets(client,user,keyword):
+    json_list = []
 
+    query_string = ''
 
+    if user != '':
+        query_string += 'from:' + user
+    if keyword != '':
+        query_string += ' ' + keyword
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    f = load_credentials()
-    data = json.load(f)
-
-    client = tweepy.Client(data['bearer_token'])
-
-    query_string = 'from:Bart_DeWever migratie -is:retweet lang:nl '
-    tweets = client.search_all_tweets(query=query_string,start_time = "2021-09-29T18:46:19Z",end_time = "2022-10-19T18:46:19Z",max_results=500,tweet_fields=['created_at','id',"author_id"])
+    query_string += ' -is:retweet lang:nl'
+    print(query_string)
+    tweets = client.search_all_tweets(query=query_string,start_time = "2022-02-15T18:46:19Z",end_time = "2023-2-15T18:46:19Z",max_results=500,tweet_fields=['created_at','id',"author_id"])
     print(len(tweets.data))
 
     for tweet in tweets.data:
         print(tweet)
         parsed_tweet = p.parse(tweet.text)
-        json_obj ={
-            "mentions": parsed_tweet.mentions,
+        json_obj = {
+            "mentions":parsed_tweet.mentions,
             "hashtags":parsed_tweet.hashtags,
             "urls":parsed_tweet.urls,
             "emojis":parsed_tweet.emojis,
@@ -41,6 +42,23 @@ if __name__ == '__main__':
             "author_id":tweet.author_id
         }
         pprint.pprint(json_obj)
+        json_list.append(json.dumps(json_obj,sort_keys=True, default=str))
+    return json_list
+
+def process_tweet(client,user,keyword):
+
+    filename = keyword if user == "" else user + '_' + keyword
+    with open(filename+'.json', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(query_tweets(client,user,keyword)))
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    f = load_credentials()
+    data = json.load(f)
+
+    client = tweepy.Client(data['bearer_token'])
+    process_tweet(client,"","migratie")
+
 
 
 
